3f3a6e7ba0217a233c36a590a8d97e9ce4795996
Steven Muñoz Vega <Darkmixt@gmail.com>
2023-08-26 23:25:11 -0600

    fix: TwoWayDataBinding fixed, Ids adds to characters

--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -4,7 +4,7 @@ import { DbzPageComponent } from './pages/main-page.component';
 import { DbzListComponent } from './components/list/list.component';
 import { DbzCharactersComponent } from './components/add-characters/characters.component';
 import { FormsModule } from '@angular/forms';
-
+import {v4} from 'uuid'
 
 
 @NgModule({
--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Character } from '../interfaces/character.interface';
+import { v4 } from 'uuid';
 
 @Component({
     selector: 'app-dbz-main-page',
@@ -9,23 +10,28 @@ import { Character } from '../interfaces/character.interface';
 export class DbzPageComponent implements OnInit {
 
     public characters: Character[] = [{
+        id: v4(),
         name : "Krillin",
         power : 500,
     },{
+        id: v4(),
         name: "Goku",
         power: 9600,
     },{
+        id: v4(),
         name: "Vegeta",
         power: 7500,
     } ];
 
 
-
     ngOnInit(){}
 
-    onNewCharacter1(character: Character):void{
-        this.characters.push(character);
-         
-    } 
+    onNewCharacter1( character: Character):void{
+
+        const newCharacter: Character = {
+          id: v4(), ...character}
+
+        this.characters.push(newCharacter);
+    }
 
 }
